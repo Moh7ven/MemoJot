14d41b6397c4434a1ac6914987e2bdb1c89c7de0
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-22 00:05:38 +0000

    redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { NoteApi } from "api/note-api";
 import { Header } from "components/Header/Header";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { setNoteList } from "store/Note/note-slice";
 
 export function App() {
+  const dispatch = useDispatch();
+  async function fetchAllNotes() {
+    const noteList = await NoteApi.fetchAll();
+    dispatch(setNoteList(noteList));
+  }
+  useEffect(() => {
+    fetchAllNotes();
+  }, []);
+
   return (
     <div className="container-fluid">
       <Header />
