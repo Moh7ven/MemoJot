e7395946e3552b95156e13c7fa6d1e99a3bc32c5
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-22 19:55:33 +0000

    params url

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,5 +1,13 @@
+import { useParams, useSearchParams } from "react-router-dom";
 import s from "./style.module.css";
+import { useSelector } from "react-redux";
 
 export function Note(props) {
-  return <div className={s.container}>Note</div>;
+  const { noteId } = useParams();
+  const [searchParams] = useSearchParams();
+  const note = useSelector((store) =>
+    store.NOTE.noteList.find((note) => note.id === noteId)
+  );
+  console.log(note);
+  return <>{searchParams.get("truc")}</>;
 }
