b69290fa16b8196a61a756cda5b84b3e1500e174
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-22 18:14:41 +0000

    NoteForm component

--- src/pages/NoteCreate/NoteCreate.jsx
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -1,5 +1,10 @@
+import { NoteForm } from "components/NoteForm/NoteForm";
 import s from "./style.module.css";
 
-export  function NoteCreate() {
-  return <div className={s.container}>NoteCreate</div>;
+export function NoteCreate() {
+  return (
+    <div className={s.container}>
+      <NoteForm title={"Create a new note"} />
+    </div>
+  );
 }
