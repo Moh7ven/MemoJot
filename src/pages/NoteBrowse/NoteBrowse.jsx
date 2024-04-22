c3b7a44114c10d0374df1bcaadd8aaa033c23a95
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-22 22:20:31 +0000

    search note

--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,10 +1,36 @@
-import s from "./style.module.css";
+import { SearchBar } from "components/SearchBar/SearchBar";
+// import s from "./style.module.css";
 import { NoteList } from "containers/NoteList/NoteList";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export function NoteBrowse() {
+  const [searchText, setSearchText] = useState("");
+
+  const noteList = useSelector((store) => store.NOTE.noteList);
+  const filteredNoteList = noteList.filter((note) => {
+    const containsTitle = note.title
+      .toUpperCase()
+      .includes(searchText.trim().toUpperCase());
+
+    const containsContent = note.content
+      .toUpperCase()
+      .includes(searchText.trim().toUpperCase());
+
+    return containsTitle || containsContent;
+  });
+
   return (
-    <div className={s.container}>
-      <NoteList />
-    </div>
+    <>
+      <div className="row justify-content-center mb-5">
+        <div className="col-sm-12 col-md-4">
+          <SearchBar
+            placeholder="Search for a note"
+            onTextChange={setSearchText}
+          />
+        </div>
+      </div>
+      <NoteList noteList={filteredNoteList} />
+    </>
   );
 }
