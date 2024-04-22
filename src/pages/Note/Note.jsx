6fe4c75db6c70fc88ec98075bd67186bf0564703
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-22 20:37:29 +0000

    update note

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,18 +1,26 @@
 import { useParams, useSearchParams } from "react-router-dom";
 import s from "./style.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NoteForm } from "components/NoteForm/NoteForm";
 import { useState } from "react";
+import { NoteApi } from "api/note-api";
+import { updateNote } from "store/Note/note-slice";
 
 export function Note(props) {
   const [isEditable, setIsEditable] = useState(false);
   const { noteId } = useParams();
+  const dispatch = useDispatch();
   // const [searchParams] = useSearchParams();
   const note = useSelector((store) =>
     store.NOTE.noteList.find((note) => note.id === noteId)
   );
 
-  function submit() {}
+  async function submit(formValues) {
+    const updatedNote = await NoteApi.update({ ...formValues, id: note.id });
+
+    dispatch(updateNote(updatedNote));
+    setIsEditable(false);
+  }
 
   return (
     <>
