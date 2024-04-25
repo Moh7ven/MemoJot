19c510faae0f14315fc43731f55cd4b6231f8313
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-25 17:33:04 +0000

    add base url production

--- src/api/note-api.js
+++ src/api/note-api.js
@@ -2,26 +2,31 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:3000/MemoJot/api/v1";
 
+const BASE_URL_PRODUCTION = "https://memojot-api.onrender.com/api/v1";
+
 export class NoteApi {
   static async create(note) {
     /* return this.formatId((await axios.post(`${BASE_URL}/addNote`, note)).data); */
-    return (await axios.post(`${BASE_URL}/addNote`, note)).data;
+    return (await axios.post(`${BASE_URL_PRODUCTION}/addNote`, note)).data;
   }
   static async fetchAll() {
-    return (await axios.get(`${BASE_URL}/getAllNotes`)).data;
+    return (await axios.get(`${BASE_URL_PRODUCTION}/getAllNotes`)).data;
   }
   static async fetchById(noteId) {
     return this.formatId(
-      (await axios.get(`${BASE_URL}/getNoteById/${noteId}`)).data
+      (await axios.get(`${BASE_URL_PRODUCTION}/getNoteById/${noteId}`)).data
     );
   }
   static async deleteById(noteId) {
-    return (await axios.delete(`${BASE_URL}/deleteNote/${noteId}`)).data;
+    return (await axios.delete(`${BASE_URL_PRODUCTION}/deleteNote/${noteId}`))
+      .data;
   }
   static async update(note) {
     /* return this.formatId(
-      (await axios.patch(`${BASE_URL}/${note.id}`, note)).data */
-    return (await axios.patch(`${BASE_URL}/updateNote/${note.id}`, note)).data;
+      (await axios.patch(`${BASE_URL_PRODUCTION}/${note.id}`, note)).data */
+    return (
+      await axios.patch(`${BASE_URL_PRODUCTION}/updateNote/${note.id}`, note)
+    ).data;
   }
 
   /* static formatId(note) {
